5bd285075f4de66ffa5c3cd9fe7829c12fd443e0
Frank Schumacher <frank.schumacher@telefonica.com>
2024-03-04 22:38:24 +0100

    Add output for obsidian

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,17 @@
 output "languagetool_api_url" {
   value = <<-EOV
+    USAGE IN CHROME EXTENSION:
+    --------------------------
     Install https://chromewebstore.google.com/detail/rechtschreibpr%C3%BCfung-%E2%80%93-lan/oldceeleldhonbafppcapldpdifcinji
-    Then open the options of the extension and scroll down to "Experimental Einstellungen (nur für Profis)" and select
+    Open the options of the extension and scroll down to "Experimental Einstellungen (nur für Profis)" and select
     "Sonstiger Server – benötigt dort einen LanguageTool-Server" and enter:
     http://${aws_lb.languagetool.dns_name}:${local.languagetool_port}/v2
+
+    USAGE IN OBSIDIAN:
+    ------------------
+    Install the "LanguageTool Integration" Community Plugin and used
+    http://${aws_lb.languagetool.dns_name}:${local.languagetool_port}
+    as the "Endpoint" Custom URL
+
   EOV
 }

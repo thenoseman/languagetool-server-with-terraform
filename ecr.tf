9194bce2c71f9687d8082a3e3c988cad33be6783
Frank Schumacher <frank.schumacher@telefonica.com>
2024-08-30 08:20:02 +0200

    repair docker push

--- ecr.tf
+++ ecr.tf
@@ -15,7 +15,7 @@ resource "null_resource" "languagetool_push_to_ecr" {
     command = <<-EOC
       aws ecr get-login-password | docker login --username AWS --password-stdin ${aws_ecr_repository.languagetool.repository_url} 
       docker pull --platform linux/amd64 ${var.languagetool_docker_image}:${var.languagetool_docker_image_tag}
-      docker tag ${var.languagetool_docker_image} ${aws_ecr_repository.languagetool.repository_url}:${var.languagetool_docker_image_tag}
+      docker tag ${var.languagetool_docker_image}:${var.languagetool_docker_image_tag} ${aws_ecr_repository.languagetool.repository_url}:${var.languagetool_docker_image_tag}
       docker push ${aws_ecr_repository.languagetool.repository_url}:${var.languagetool_docker_image_tag}
     EOC
   }

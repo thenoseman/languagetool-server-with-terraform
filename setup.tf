bd830832cca3968134a92fde2d59948cc4a39b57
Frank Schumacher <frank.schumacher@telefonica.com>
2025-12-03 11:22:36 +0100

    Update project

--- setup.tf
+++ setup.tf
@@ -6,14 +6,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5"
+      version = ">= 6"
     }
     null = {
       source  = "hashicorp/null"
       version = ">= 3"
     }
   }
-  required_version = ">= 1.12.0"
+  required_version = ">= 1.14.0"
 }
 
 provider "aws" {
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "languagetool_docker_image" {
 
 variable "languagetool_docker_image_tag" {
   type    = string
-  default = "6.6-5"
+  default = "6.7-3"
 }
 
 variable "languages" {
